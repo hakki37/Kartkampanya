96cfb044aa211d408be294c24e178b8881e8377c
Nodefaster37 <103541702+hakki37@users.noreply.github.com>
2026-08-16 22:48:09 +0300

    Add files via upload

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,5 @@
 class AppConfig {
   static const supabaseUrl = 'https://bwkfehgzupbewfeovfnb.supabase.co';
-  // GitHub Actions secret olarak SUPABASE_PUBLISHABLE_KEY tanımlanacak.
   static const supabasePublishableKey =
       String.fromEnvironment('SUPABASE_PUBLISHABLE_KEY');
 }
