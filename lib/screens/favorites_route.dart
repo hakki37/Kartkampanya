83d54d6a4b73df7f322e46aaf6d7cab0cad14b6e
Nodefaster37 <103541702+hakki37@users.noreply.github.com>
2026-09-12 01:45:36 +0300

    Add favorites route

---
+++ lib/screens/favorites_route.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'favorites_screen.dart';
+
+class FavoritesRoute extends StatelessWidget {
+  const FavoritesRoute({super.key});
+  @override
+  Widget build(BuildContext context) => const FavoritesScreen();
+}
